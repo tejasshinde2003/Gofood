c33810b40d7135d1fe1746dde3d7196be66dfd82
Tejass2712 <140151256+Tejass2712@users.noreply.github.com>
2023-08-08 13:00:27 +0530

    Navbar

---
+++ src/screens/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+
+export default function Home() {
+  return (
+    <div>
+     <div><Navbar></Navbar></div> 
+     <div>body</div>
+     <div> <footer/> </div>
+    </div>
+  )
+}
