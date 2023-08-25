532d66aff78de249de7d3ddcc88496883e3fe6cc
Tejass2712 <140151256+Tejass2712@users.noreply.github.com>
2023-08-26 01:44:58 +0530

    loginUser

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,16 @@ const express = require('express')
 const app = express()
 const port = 5000
 const mongoDB = require("./db")
+
+app.use((req,res,next)=>{
+res.setHeader("Access-Control-Allow-Origin","https://localhost:3000");
+res.header(
+  "Access_Control-Allow-Headers",
+  "Origin, X-Requested-Width, Content-Type, Accept"
+);
+next();
+})
+
 mongoDB();
 app.get('/', (req, res) => {
   res.send('Hello !')
